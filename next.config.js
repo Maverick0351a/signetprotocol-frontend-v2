2ac0930f515562d08556068b97641f882d1e5ddf
Maverick <travjohnson831@gmail.com>
2025-08-29 16:26:05 -0700

    chore: disable typedRoutes to fix build until all routes recognized

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true, },
+  // typedRoutes disabled temporarily to unblock build (Vercel was building older commit without all pages)
 }
 
 module.exports = nextConfig
